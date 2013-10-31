0dee8af553179720d81ad25d293a1d578caee61c
aerych <aerychj@gmail.com>
2013-10-31 11:27:07 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,37 @@
+# Android Emulator Builder
+
+Android Emulator Builder is a ruby script designed to quickly install a set of pre-configured emulators of popular Android devices.
+
+Created emulators match the screen size and pixel density of their target devices based on device information available on Wikipeda.
+
+- [List of displays by pixel density](http://en.wikipedia.org/wiki/List_of_displays_by_pixel_density)
+- [Comparison of Android devices](http://en.wikipedia.org/wiki/Comparison_of_Android_devices)
+
+Other hardware settings are based on common configuration. The reasoning behind this is to create emulators with tolerable performance and still be reasonably useful for testing web and app UIs.
+
+##Requirments:
+Running this script requires:
+
+- Mac OS X
+- Ruby 1.9.x or later
+- An internet connection.
+- An installed copy of the [Android SDK](http://developer.android.com/sdk/index.html)
+
+##Usage
+
+1. Download and unzip this project or clone the repo 
+2. Launch a terminal window
+3. cd to where the script is located
+4. Run the script in a terminal window by typing 
+
+	$> ruby emulators.rb
+
+###Configuring Emulators
+
+The normal operation of the script is to configure a stock set of emulators. You will be prompted to confirm this action. The script will inspect your local copy of the Android SDK and attempt to install any missing packages.  Finally it will configure the emulators, and launch the Android Virtual Device manager.
+Note that running this script will overwrite any previously created emulators with the same name.
+
+###Installing WordPress for Android
+
+If you run this script while there is a running emulator, you will be prompted to install the latest copy of WordPress for Android.  If you answer (y) the application .apk will be downloaded to your working directory and installed in the running emulator. Once its installed it will be launched automatically.
+
---
+++ emulators.rb
@@ -0,0 +1,255 @@
+#!/usr/bin/env ruby
+require 'yaml'
+
+# Configure a stock set of Android Emulators based on settings defined in emulators.yml. 
+# Emulators use the same hardware settings with the exception of screen size, and density.
+class EmulatorBuilder
+  
+  @@packages = [
+    'Android SDK Tools, revision 22.2.1',
+    'Android SDK Platform-tools, revision 18.0.1',
+    'Android SDK Build-tools, revision 18.1.1',
+    'SDK Platform Android 4.3, API 18, revision 2',
+    'Google APIs, Android API 18, revision 3',
+    'SDK Platform Android 4.0.3, API 15, revision 3',
+    'Google APIs, Android API 15, revision 2',
+    'SDK Platform Android 2.3.3, API 10, revision 2',
+    'Google APIs, Android API 10, revision 2'
+  ]
+  
+  
+  @@config = [
+    'skin.dynamic=yes',
+    'hw.accelerometer=yes',
+    'hw.audioInput=yes',
+    'hw.battery=yes',
+    'hw.camera.back=none',
+    'hw.camera.front=none',
+    'hw.dPad=yes',
+    'hw.gps=yes',
+    'hw.keyboard=yes',
+    'hw.mainKeys=yes',
+    'hw.ramSize=1024',
+    'hw.sdCard=no',
+    'hw.sensors.orientation=yes',
+    'hw.sensors.proximity=yes',
+    'hw.trackBall=no',
+    'vm.heapSize=128'
+  ]
+  
+  
+  def init
+    check_sdk
+    
+    # If an emulator is running prompt to install the latest apk.
+    if @adb_path
+      str = `#{@adb_path} devices`
+      str = str.strip.split("\n").pop
+      if str.include?("emulator")
+        install
+        return
+      end
+    end
+    
+    build
+  end
+
+
+  def build
+    puts "\n\nHi there!\n\n" 
+    puts "Running this script will set up some preconfigured Android emulators."
+    puts "Any required components (like the Android SDK) will also be downloaded and configured."
+    puts "It might take a while. ;)\n\n"
+    puts "Do you want to continue? y/n :"
+
+    proceed = gets.strip
+    if proceed != "y"
+      puts "\nBye\n\n"
+      return
+    end
+    
+    puts "\nGreat! Getting started.\n\n"
+    
+    if @sdk_path.nil?
+      puts "\nAndroid SDK missing. Please install the Android SDK."
+      return
+      # TODO
+      # install_sdk
+    end
+    
+    packages = check_missing_components
+    if (packages.length > 0)
+      install_missing_components(packages)
+    end
+
+    puts "Configuring Virtual Devices..."
+    configure_emulators
+    
+    puts "Finished"
+    spawn "#{@sdk_path} avd" #launch the virtual device manager
+    
+  end
+
+
+  # Check the specified path for the location of the Android SDK.
+  # Returns a matching substring or nil.
+  def check_sdk_path(path)
+    users_dir = '/Users'
+    sdk_dir = 'android-sdks'
+    android_path = '/tools/android'
+    adb_path = '/platform-tools/adb'
+    
+    idx = path.rindex(sdk_dir)
+    if idx.nil?
+      return
+    end
+    path = path[0, idx + sdk_dir.length]
+
+    idx = path.rindex(users_dir)
+    path = path[idx, path.length]
+    
+    if !File.directory?(path)
+      return
+    end
+    
+    tool_path = path + android_path
+    if File.exists?(tool_path)
+      @sdk_path = tool_path
+    end
+    
+    tool_path = path + adb_path
+    if File.exists?(tool_path)
+      @adb_path = tool_path
+    end
+    
+  end
+
+  
+  # Look for the SDKs Android tool
+  def check_sdk
+    puts "Looking for the Android SDK"
+    
+    # Look for the Android SDK. If we can't find it, ask for one. 
+    sdk_path = check_sdk_path(`which android`)
+
+    if sdk_path.nil?
+      sdk_path = check_sdk_path(ENV['ANDROID_HOME'])
+    end
+
+    if sdk_path.nil?
+      sdk_path = check_sdk_path(ENV['PATH'])
+    end
+    
+    if sdk_path.nil?
+      #hail mary
+      sdk_path = check_sdk_path(ENV['HOME'] + '/android-sdks')
+    end
+    
+    if sdk_path.nil?
+      #wah wah waaah...
+      return
+    end
+
+    sdk_path
+  end
+  
+  
+  # Check installed packages an confirm we have everything we need to build our emulators. 
+  def check_missing_components
+    puts "Checking installed components... \n"
+    
+    arr = []
+    unpkgs = `#{@sdk_path} list sdk -u`
+    unpkgs = unpkgs[unpkgs.index('Packages available for installation'), unpkgs.length]
+    pkgs = unpkgs.split("\n")
+
+    # for each uninstalled package, check it against our list of required packages. 
+    # if found, save its id, we need to install it
+    for item in pkgs
+      for pkg in @@packages
+        if item.index(pkg)
+          arr << item.strip.to_i.to_s
+          break
+        end
+      end
+    end
+    
+    arr
+  end
+  
+  
+  # Try to install any missing packages
+  def install_missing_components(packages)
+    puts "Installing missing packages...\n\n"
+    packages = packages.join(",")
+    system("#{@sdk_path} update sdk --no-ui --filter #{packages}")
+  end
+  
+  
+  # Configure emulators.  Overwrites existing emulators with the same name.
+  def configure_emulators
+    emulators = YAML::load_file(File.join(File.dirname(File.expand_path(__FILE__)), 'emulators.yml'))
+    # create each emulator in the list
+    # after the emulator is created, edit its config to use our desired settings
+    for emulator in emulators
+      puts "Configuring : #{emulator['name']}\n"
+      args = " --abi #{emulator['abi']} --skin #{emulator['skin']} --target #{emulator['target']} --name #{emulator['name']}"
+      # Echo no, to decline setting up a custom hardware profile. 
+      `echo no | android create avd --force --snapshot #{args}`
+      update_emulator_config(emulator)
+    end
+  end
+
+
+  # Updates the emulator's config.ini with the desired settings.
+  def update_emulator_config(emulator)
+    # find the config file for the emulator
+    path = ENV['HOME'] + '/.android/avd/' + emulator['name'] + '.avd/config.ini'
+    if !File.exists?(path)
+      return
+    end
+    
+    # prep the config
+    str = ''
+    config = emulator['config']
+    config.each {|k,v|
+      str = str + "\n#{k}=#{v}"
+    }
+
+    doc = @@config.join("\n")
+    doc = doc + str
+    
+    # open for appending
+    File.open(path, 'a') { |f|
+      f.write(doc)
+    }
+  end
+
+
+  # Install the latest version of WordPress for Android on a running emulator
+  def install
+    puts "\n\nHi there!\n\n" 
+    puts "I've detected a running emulator."
+    puts "Would you like to install the latest version of WordPress or Android? y/n"
+
+    proceed = gets.strip
+    if proceed != 'y'
+      puts "\nBye\n\n"
+      return
+    end
+    
+    puts "Downloading WordPress for Android \n"
+    system("curl -o wpandroid.apk http://iosbuilds.automattic.com/builds/android/latest.apk")
+    
+    puts "Installing WordPress for Android \n"
+    system("#{@adb_path} install wpandroid.apk")
+    
+    system("#{@adb_path} shell am start -n org.wordpress.android/org.wordpress.android.ui.posts.PostsActivity")
+    puts "Finished"
+  end
+
+
+end
+
+builder = EmulatorBuilder.new
+builder.init
